69dc7048aeb3175f519358c726f26d5805c7e18c
carla242 <njohcarla26@gmail.com>
2025-04-14 22:18:36 +0200

    simple fonction mathématique

---
+++ main.js
@@ -0,0 +1,6 @@
+const { sum, diff, prod, quot } = require('./math');
+
+console.log("Addition: ", sum(7, 5));
+console.log("Soustraction: ", diff(10, 5));
+console.log("Multiplication: ", prod(9, 5));
+console.log("Division: ", quot(15, 5));
---
+++ math.js
@@ -0,0 +1,22 @@
+function sum(a, b) {
+    return a + b;
+  }
+  
+  function diff(a, b) {
+    return a - b;
+  }
+  
+  function prod(a, b) {
+    return a * b;
+  }
+  
+  function quot(a, b) {
+    return a / b;
+  }
+  
+  module.exports = {
+    sum,
+    diff,
+    prod,
+    quot
+  };
